44cb2e8a08245eb55d4c2bfa2999f98ea6fa4243
Thomas Marc Beetz <siteraffer@gmail.com>
2017-09-13 18:51:11 +0200

    Work on redux reducer
    added store and provider from react-redux and redux.
    createStore and applyMiddleware added from redux to create store which is provided to the single state of truth via react
    redux <Provider> with jsx property store

---
+++ config/keys.js
@@ -0,0 +1,2 @@
+// const weatherAPI = 'ff84f45749b2a4665cf37312097a278b';
+// api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 //Take this component's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector('.container')
+);
