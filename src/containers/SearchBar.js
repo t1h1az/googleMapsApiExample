d25e3fb61c91b2401542c7e5d6f9b142be464271
Thomas Marc Beetz <siteraffer@gmail.com>
2017-09-13 17:50:29 +0200

    Add Searchbar container to app component
    input has event handler. the event handler is bound to component state.
    this.state is console.logged
    working

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' }
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({term: event.target.value});
+
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Search for city"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+          />
+        <p>{this.state.term}</p>
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
