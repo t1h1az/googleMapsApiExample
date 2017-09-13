0c8be1b8a1d40e6dc05cbdcf496fd7c1d10876ac
Thomas Marc Beetz <siteraffer@gmail.com>
2017-09-13 19:28:52 +0200

    Add Axios async request to action creator
    add get request with weather api url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,18 @@
 const API_KEY = 'ff84f45749b2a4665cf37312097a278b';
 const API_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 const countryCode = 'us';
+import axios from 'axios';
 // extracted because we want to keep our action types consistent between
 // action creators and reducerss
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather (city) {
   const returnUrl = `${API_URL}&q=${city},${countryCode}`;
+  const requestWeather = axios.get(returnUrl);
 
   return (
     type: FETCH_WEATHER,
-    payload: this.
+    // payload can contain additional data that describes particular action
+    payload: requestWeather
   );
 }
