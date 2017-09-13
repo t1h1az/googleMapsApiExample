34c5b9015b43ef34c07f2291e71c4564bb9c9e66
Thomas Marc Beetz <siteraffer@gmail.com>
2017-09-13 17:21:42 +0200

    Add simple searchbar container to app component, no functioniality yet
    desc: nothing special

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SearchBar from '../containers/SearchBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+export default App;
