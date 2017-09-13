dd59a181722a09300f1cf745a152c3f4beed2fb7
Thomas Marc Beetz <siteraffer@gmail.com>
2017-09-13 20:59:12 +0200

    Hooked up action creator with component search bar
    action creator fetchWeather makes an axios request for a json object
    next step is to pipe data from request to weather reducer function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,21 @@
-const API_KEY = 'ff84f45749b2a4665cf37312097a278b';
-const API_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
-const countryCode = 'us';
 import axios from 'axios';
+
+const API_KEY = 'ff84f45749b2a4665cf37312097a278b';
+const API_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const countryCode = 'us';
 // extracted because we want to keep our action types consistent between
 // action creators and reducerss
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather (city) {
-  const returnUrl = `${API_URL}&q=${city},${countryCode}`;
-  const requestWeather = axios.get(returnUrl);
+export function fetchWeather(city) {
+  const url = `${API_URL}&q=${city},us`;
+  axios.get(url)
+    .then((res) => res);
 
-  return (
+  const req = axios.get(url);
+
+  return {
     type: FETCH_WEATHER,
-    // payload can contain additional data that describes particular action
-    payload: requestWeather
-  );
+    payload: req
+  };
 }
--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {  fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+
+
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: '' }
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
     this.setState({term: event.target.value});
+    console.log(this.props);
 
   }
   onFormSubmit(event) {
@@ -17,9 +24,8 @@ export default class SearchBar extends Component {
     // if you press enter on your keyboard
     // the form is not submitted
     event.preventDefault();
-  }
-  onButtonSubmit(event) {
-    
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
   }
 
   render() {
@@ -37,10 +43,24 @@ export default class SearchBar extends Component {
           <button
             type="submit"
             className="btn btn-secondary"
-            onSubmit={this.onButtonSubmit}>Submit</button>
+            >Submit</button>
         </span>
       </form>
     );
   }
-
 }
+
+
+//  hook up actioncreater to container
+// call to bindActionCreators
+// action creator flows through middleware to reducer
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather },dispatch);
+}
+
+// null container doesnt care about state
+// map our dispatch to the props of our container
+// first we bind fetchWeather to dispatch and
+// by mapping dispatch to props we can access the function fetchWeather
+// over our this.props.fetchWeather
+export default connect(null, mapDispatchToProps)(SearchBar);
