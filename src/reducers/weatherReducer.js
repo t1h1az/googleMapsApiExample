a81f9e748bba8e0fce2b047348a080740c122058
Thomas Marc Beetz <siteraffer@gmail.com>
2017-09-14 08:36:33 +0200

    Add weather reducer, async server request working correctly

--- src/reducers/weatherReducer.js
+++ src/reducers/weatherReducer.js
@@ -1,3 +1,17 @@
-export default function() {
-  return {loc: 'unknown'};
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [null], action) {
+    switch(action.type){
+      case FETCH_WEATHER:
+        // state is mutated directly
+        // return state.push(action.payload.data);
+        //possible return state.concat([action.payload.data])
+
+        //ES6 now. whats happening here
+        // ...state takes all entrys from old array and puts it into new array
+
+        return [action.payload.data, ...state]
+      default:
+        return state;
+    }
 };
