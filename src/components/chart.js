5fcc191cc46904de46297923ef9dfb54a678406c
Thomas Marc Beetz <siteraffer@gmail.com>
2017-09-14 12:27:45 +0200

    Add Sparkline to display charts and made a new chart component

---
+++ src/components/chart.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+export default (props) => {
+  return (
+    <Sparklines height={120} width={180} data={props.data}>
+      <SparklinesLine color="red" />
+    </Sparklines>
+  );
+};
--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,14 +1,65 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
+  constructor(props) {
+    super(props);
+    this.humidityMedian = this.humidityMedian.bind(this);
+    this.renderWeatherData = this.renderWeatherData.bind(this);
+
+  }
+
   renderWeatherData(cityData){
+    const name = cityData.city.name;
+    const temps = cityData.list.map((weather) => weather.main.temp);
+
        return (
-         <tr key={cityData.city.name}>
-           <td >{cityData.city.name}</td>
+         <tr key={name}>
+           <td>{name}</td>
+           <td>
+              <Chart data={temps} />
+           </td>
          </tr>
        );
   }
+
+  humidityMedian(cityData) {
+    console.log(cityData);
+    let cummulatedHumidity = 0;
+    cityData.list.map((item) => {
+      cummulatedHumidity = (cummulatedHumidity + item.main.humidity);
+    });
+    return Math.round(cummulatedHumidity/cityData.list.length*100)/100;
+  }
+
+  temperatureMedian(cityData) {
+    console.log(cityData);
+    let cummulatedTemperature = 0;
+    cityData.list.map((item) => {
+      cummulatedTemperature = (cummulatedTemperature + item.main.temp);
+    });
+    return Math.round(cummulatedTemperature/cityData.list.length*100)/100;
+  }
+
+
+    pressureMedian(cityData) {
+      console.log(cityData);
+      let cummuluatedPressure = 0;
+      cityData.list.map((item) => {
+        cummuluatedPressure = (cummuluatedPressure + item.main.pressure);
+      });
+      return Math.round(cummuluatedPressure/cityData.list.length*100)/100;
+    }
+
+
+  fetchTemperatur(cityData){
+    cityData.list.map((item) => {
+      cummulatedHumidity = (cummulatedHumidity + item.main.humidity);
+    });
+  }
+
 
   render() {
     return (
